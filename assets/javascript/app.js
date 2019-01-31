c5667d18a3a8e574afb9c71bf7add855b6592dad
Tim Welsh <tim@timwelsh.com>
2019-01-31 17:05:48 -0500

    more moment code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,46 +27,48 @@ $( document ).ready(function() {
         freq = snapshot.val().freq;
         formattedFirstTime = moment(trainTime, 'hh:mm').format("x")
         interval = freq * 60000;  // take frequency in minutes and multiple to get the unix number for minutes in milliseconds
-
-        
-        formattedTime + 
-
-        while(moment.now() < (formattedFirstTime + interval)) {
-            $('#next-display').text(duration.hours()+ 'h:' + duration.minutes()+ 'm:');
-        } else {
-            // add another freq to the start time
-
-        }
-        formattedTime = (moment(trainTime, 'hh:mm').format("x") / 60000)
-        fromNow = (moment.now() / 60000)
+        minAway = moment.duration(interval).humanize()
+        // moment.duration(416000).humanize();
+        console.log(formattedFirstTime + " formattedFirstTime " + trainTime)
+        // formattedTime + 
+        $('#next-display').text(formattedFirstTime)
+        $('#min-display').text(minAway)
+        // while(moment.now() < (formattedFirstTime + interval)) {
+        //     $('#next-display').text(moment.now())  //.text(duration.hours()+ 'h:' + duration.minutes()+ 'm:');
+        // } 
+        //     // add another freq to the start time
+        //     $('#next-display').text(moment.now())
+        // }
+        // formattedTime = (moment(trainTime, 'hh:mm').format("x") / 60000)
+        // fromNow = (moment.now() / 60000)
         // minutesRemaining = formattedTime + (freq )
         // console.log(formattedTime + " here " + trainTime + " from now " + fromNow)
         // console.log(calcDuration + " calc Duration " + freq)
         
-        var convertedTrainTime = '1366549200';
-        var currentTime = '1366547400';
-        var leftTime = eventTime - currentTime;//Now i am passing the left time from controller itself which handles timezone stuff (UTC), just to simply question i used harcoded values.
-        var duration = moment.duration(leftTime, 'seconds');
-        var interval = calcDuration;
+        // var convertedTrainTime = '1366549200';
+        // var currentTime = '1366547400';
+        // var leftTime = eventTime - currentTime;//Now i am passing the left time from controller itself which handles timezone stuff (UTC), just to simply question i used harcoded values.
+        // var duration = moment.duration(leftTime, 'seconds');
+        // var interval = calcDuration;
     
-        setInterval(function(){
-          // Time Out check
-          if (duration.asSeconds() <= 0) {
-            clearInterval(intervalId);
-            // window.location.reload(true); //#skip the cache and reload the page from the server
-          }
+        // setInterval(function(){
+        //   // Time Out check
+        //   if (duration.asSeconds() <= 0) {
+        //     clearInterval(intervalId);
+        //     // window.location.reload(true); //#skip the cache and reload the page from the server
+        //   }
 
-          //Otherwise
-          duration = moment.duration(duration.asSeconds() - 1, 'seconds');
-          $('#next-display').text(duration.hours()+ 'h:' + duration.minutes()+ 'm:');
-        }, interval);
+        //   //Otherwise
+        //   duration = moment.duration(duration.asSeconds() - 1, 'seconds');
+        //   $('#next-display').text(duration.hours()+ 'h:' + duration.minutes()+ 'm:');
+        // }, interval);
 
 
         $("#train-display").append(train + "<br />");
         $("#destination-display").append(destination + "<br />");
         $("#freq-display").append(freq + "<br />");
         $("#trainTime-display").append(trainTime + "<br />");
-        $("#next-display").append(months + "<br />");
+        // $("#next-display").append(months + "<br />");
     
     // }, function(errorObject) {
     // console.log("Error: " + errorObject.code);
